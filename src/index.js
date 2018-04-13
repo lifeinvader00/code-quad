b97adee82bed602cc179f1fedc48d66fe792dfdd
Abhinav Ramkumar <abhinavramkumar91@gmail.com>
2018-04-13 23:47:10 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+
+const App = () => <div>Code Quad</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
